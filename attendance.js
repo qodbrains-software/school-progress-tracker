43c3e4ab0b781a1e21686de6d229ad627170dcfc
sonwab <sona@qodbrains.com>
2022-06-30 15:36:46 +0200

    attendance style and date

---
+++ attendance.js
@@ -0,0 +1,12 @@
+let date = document.querySelector(".date");
+
+const newDate = () => {
+  let today = new Date();
+  dd = today.getDay();
+  mm = today.getMonth() + 1;
+  yyyy = today.getFullYear();
+
+  return "0" + dd + "-" + "0" + mm + "-" + yyyy;
+};
+
+date.textContent = newDate();
