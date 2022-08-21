30f2b14cf2617077fc1e39d18752d469bc254698
vusumzi-qod <vusumzi@qodbrains.com>
2022-08-21 18:43:43 +0200

    fixing studend dash board and search logic

---
+++ mockData.js
@@ -0,0 +1,206 @@
+// // ==================================================================================================================
+// //                                                   Mock data
+// // ==================================================================================================================
+
+// const studentList = {
+//     students: [
+//       {
+//         student_name: "Christopher Jimenez",
+//         attendance: [],
+//         age: 16,
+//         gender: "Male",
+//         term: 1,
+//         subjects: [
+//           {
+//             english: [
+//               {
+//                 task: "reading",
+//                 marks: 10,
+//                 comment:""
+//               },
+//               {
+//                 task: "speech",
+//                 marks: 15,
+//                 comment:""
+//               },
+//               {
+//                 task: "test",
+//                 marks: 25,
+//                 comment:""
+//               },
+//               {
+//                 task: "exam",
+//                 marks: 50,
+//                 comment:""
+//               }
+//             ],
+//             maths: [
+//               {
+//                 task: "project",
+//                 marks: 25,
+//                 comment:""
+//               },
+//               {
+//                 task: "test",
+//                 marks: 25,
+//                 comment:""
+//               },
+//               {
+//                 task: "exam",
+//                 marks: 50,
+//                 comment:""
+//               }
+//             ]     
+//           }
+//         ]
+//       },
+//       {
+//         student_name: "Dannielle Wright",
+//         attendance: [],
+//         age: 18,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Danielius Ahmad",
+//         attendance: [],
+//         age: 17,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Verity Hammond",
+//         attendance: [],
+//         age: 16,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Talia Arroyo",
+//         attendance: [],
+//         age: 16,
+//         gender: "Female",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Katya Randall",
+//         attendance: [],
+//         age: 18,
+//         gender: "Female",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Lennie Williamson",
+//         attendance: [],
+//         age: 17,
+//         gender: "Female",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Autumn Burrows",
+//         attendance: [],
+//         age: 16,
+//         gender: "Female",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Mcauley Reed",
+//         attendance: [],
+//         age: 20,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Ralph Lozano",
+//         attendance: [],
+//         age: 19,
+//         gender: "Female",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Arfa Buxton",
+//         attendance: [],
+//         age: 16,
+//         gender: "Female",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Rafael Prosser",
+//         attendance: [],
+//         age: 16,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Lena Lacey",
+//         attendance: [],
+//         age: 18,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Haydn Huffman",
+//         attendance: [],
+//         age: 19,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Lulu Maddox",
+//         attendance: [],
+//         age: 20,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Zacharias Dickens",
+//         attendance: [],
+//         age: 19,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Kiri Fletcher",
+//         attendance: [],
+//         age: 16,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Asma Castillo",
+//         attendance: [],
+//         age: 19,
+//         gender: "Female",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Reegan Nash",
+//         attendance: [],
+//         age: 17,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Dianne Knox",
+//         attendance: [],
+//         age: 18,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Avani Knott",
+//         attendance: [],
+//         age: 16,
+//         gender: "Male",
+//         term: 1,
+//       },
+//       {
+//         student_name: "Maheen Bravo",
+//         attendance: [],
+//         age: 18,
+//         gender: "Male",
+//         term: 1,
+//       },
+  
+//     ]
+//   };  
